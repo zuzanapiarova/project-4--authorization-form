82f1206cf922f8bfb8368893a97e0526505b6bfb
Zuzana Piarová <zuzana.piarova1@gmail.com>
2022-12-29 19:38:00 +0100

    javascript with complete code for password authorization

--- main.js
+++ main.js
@@ -21,9 +21,13 @@ class Contact extends React.Component {
   }
 
   render() {
-    return (
-      <div id="authorization">
-        <h1>Contact</h1>
+    const login = (
+      <form action='#' onSubmit={this.authorize}>
+        <input type='password' placeholder='Password'/>
+        <input type='submit' />
+      </form>
+    );
+    const contactInfo = (
         <ul>
           <li>
             client@example.com
@@ -32,6 +36,11 @@ class Contact extends React.Component {
             555.555.5555
           </li>
         </ul>
+    );
+    return (
+      <div id="authorization">
+        <h1>{ this.state.authorized ? 'Contact' : 'Enter the Password' }</h1>
+       { this.state.authorized ? contactInfo : login }
       </div>
     );
   }
